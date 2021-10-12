705045787004a02065296c978599b581338424bb
hpurring <hilarypurrington@gmail.com>
2021-10-11 21:56:56 -0400

    add while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var enemyAttack = 12;
 var playerMoney = 10;
 
 var fight = function(enemyName) {
+    while(enemyHealth > 0) { 
     // alert players they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -47,7 +48,10 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
     }
 }
+}
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
